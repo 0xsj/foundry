9207e50ed20efa07a2caa1b4f547eba849472b66
sjtommylee <sjtommylee@gmail.com>
2023-08-21 16:02:53 -0700

    chore: commit save point

--- valid-anagram/solution.ts
+++ valid-anagram/solution.ts
@@ -7,7 +7,11 @@ const pair = [word1, word2];
 const pair2 = [word3, word4];
 
 /**
- *
+ * 1. we just compare two strings that we run maniulates through.
+ * 2. we split the string, sort it, which would give us a specific sequence of characters
+ * 3. we then join the characters array that is sorted to complete / form a new string.
+ * 4. we do the same for the second argument / string
+ * 5. if the two strings match, it means that it is an anagram
  */
 function bruceForce(s: string, t: string, args?: string[]): boolean {
   return s.split("").sort().join("") === t.split("").sort().join("");
