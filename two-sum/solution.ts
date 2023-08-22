c55083d120fffed9b4d828da872b697ef803a2c1
sjtommylee <sjtommylee@gmail.com>
2023-08-22 14:46:15 -0700

    hashmap

--- two-sum/solution.ts
+++ two-sum/solution.ts
@@ -18,4 +18,29 @@ const bruteForce = (nums: number[], target: number): number[] => {
   return [-1, 1];
 };
 
-console.log(bruteForce(nums, target));
+/**
+ * 1. we create a new empty map called indicies
+ * 2. we start a for loop, go until the full length of the array.
+ * 3. we look for a diff. this calculates the difference between the target and the current element in the nums array
+ * 4. essentially, we just need to figure out what number we need to add to the current number in order to get to the target.
+ * 5. the if block, we are just checking if we have seen a number in the nums array that when added to the current number, it would equal the target.
+ * 6. if the indicies contain a diff value as a key, it means that there is a number in the nums array that meets our conditions
+ * 7. we return an array containing two indicies.
+ * - diff! - gets the index frosm the indicides map that corresponds to the complement diff.
+ * - i - current index in the nums array , where we found the current number nums[i]
+ *
+ */
+const hashMap = (nums: number[], target: number): number[] => {
+  const indicies = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (indicies.has(diff)) {
+      return [indicies.get(diff)!, i];
+    }
+    indicies.set(nums[i], i);
+  }
+  return [];
+};
+
+console.log("hashmap : ", hashMap(nums, target));
+console.log("bruteforce  : ", bruteForce(nums, target));
