73acb737f470a91a8ccde55b40a318a75f69e0dc
sjtommylee <sjtommylee@gmail.com>
2023-08-22 20:08:06 -0700

    chore: commit save point

--- javascript/apply-transform/solution.ts
+++ javascript/apply-transform/solution.ts
@@ -13,6 +13,12 @@ class ApplyTransform {
     this.fn = fn;
   }
 
+  /**
+   * 1. declare a new array results
+   * 2. loop though the array that is passed in as args
+   * 3. push each element back into the array and pass in the callback with the position and value as args
+   * 4. return the results;
+   */
   arrayPush(): number[] {
     const { arr, fn } = this;
     const results: number[] = [];
@@ -22,8 +28,18 @@ class ApplyTransform {
     return results;
   }
 
+  /**
+   * 1. we do the same thing as the array push
+   * 2. this time we use forEach. The forEach() method of Array instances executes a provided function once for each array element.
+   * 3. the results.push will execute every time for each iterable
+   */
   arrayForEach(): number[] {
-    return [];
+    const { arr, fn } = this;
+    const results: number[] = [];
+    arr.forEach((value, index) => {
+      results.push(fn(value, index));
+    });
+    return results;
   }
   arrayReduce(): number[] {
     return [];
@@ -48,3 +64,4 @@ class ApplyTransform {
 const test = new ApplyTransform(arr, add);
 
 console.log(test.arrayPush());
+console.log(test.arrayForEach());
