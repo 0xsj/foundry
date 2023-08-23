743e1b91f0bcb5ee47ec655efb8dddcc6997f503
sjtommylee <sjtommylee@gmail.com>
2023-08-22 19:59:52 -0700

    for

--- javascript/apply-transform/solution.ts
+++ javascript/apply-transform/solution.ts
@@ -4,10 +4,33 @@ const add = function plusone(n: any) {
   return n + 1;
 };
 
-const genericForloop = (arr: number[], fn: (n: number, i: number) => number): number[] => {
-  const results: number[] = [];
-  for (let i = 0; i < arr.length; i++) {
-    results.push(fn(arr[i], i));
+class ApplyTransform {
+  arr: number[];
+  fn: (n: number, i: number) => number;
+
+  constructor(arr: number[], fn: (n: number, i: number) => number) {
+    this.arr = arr;
+    this.fn = fn;
   }
-  return results;
-};
+
+  arrayPush(): number[] {
+    const { arr, fn } = this;
+    const results: number[] = [];
+    for (let i = 0; i < arr.length; i++) {
+      results.push(fn(arr[i], i));
+    }
+    return results;
+  }
+
+  // arrayForEach(): {}
+  // arrayReduce() {}
+  // arrayFrom() {}
+  // arrayForOf {}
+  // arrayFilter {}
+  // recursive {}
+  // flatMap
+}
+
+const test = new ApplyTransform(arr, add);
+
+console.log(test.arrayPush());
