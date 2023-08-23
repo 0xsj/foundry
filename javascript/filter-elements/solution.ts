f26d222c80810b864b2f383679f1d9a67c67e747
sjtommylee <sjtommylee@gmail.com>
2023-08-23 14:57:54 -0700

    filter

--- javascript/filter-elements/solution.ts
+++ javascript/filter-elements/solution.ts
@@ -1,5 +1,25 @@
-function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
+const num = [1, 2, 3];
+const num2 = [0, 10, 20, 30];
+
+const addCB = function plusone(n: any) {
+  return n + 1;
+};
+
+const greaterThanCB = (n: number) => {
+  return n > 10;
+};
+
+class FilterElements {
+  arr: number[];
+  fn: (n: number, i: number) => any;
+  constructor(arr: number[], fn: (n: number, i: number) => any) {
+    this.arr = arr;
+    this.fn = fn;
+  }
+}
+
+function filtered_for(arr: number[], fn: (n: number, i: number) => any): number[] {
   return arr.filter((num, index) => fn(num, index));
 }
 
-console.log(filter(arr, add));
+console.log(filtered_for(num2, greaterThanCB));
