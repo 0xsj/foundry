7f090f90ab4040e8021e651bc6958d78bf8d9749
sjtommylee <sjtommylee@gmail.com>
2023-08-28 14:58:11 -0700

    chore: commit save point

--- two-sum/solution.go
+++ two-sum/solution.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func twoSum_bruceForce(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
 func main() {
-	fmt.Println("1111")
+	array1 := []int{2, 7, 3, 5}
+	array2 := []int{3, 6, 1, 0}
+
+	target := 9
+
+	fmt.Println(twoSum_bruceForce(array1, target))
 }
