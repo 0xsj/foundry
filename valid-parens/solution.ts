eb00dc8e5f02728e69f9cd12dee7f3364bf1770a
sjtommylee <sjtommylee@gmail.com>
2023-08-28 15:13:36 -0700

    chore: commit save point

--- valid-parens/solution.ts
+++ valid-parens/solution.ts
@@ -2,8 +2,30 @@ const s = "()";
 const t = "(]";
 const y = "()[]()";
 
-function isValid(s: string): boolean {
-  //
+// valid parens, must have open and closing.
+// open and closing must be specific
 
+// first approach
+/**
+ * 1. create a look up table or a key value pair store, where we associate () {} []
+ * 2.
+ */
+
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+  const parenMap: Map<string, string> = new Map([
+    ["(", ")"],
+    ["{", "}"],
+    ["[", "]"],
+  ]);
+
+  for (const char of s) {
+    if (parenMap.has(char)) {
+      stack.push(char);
+      console.log(stack);
+    }
+  }
   return false;
 }
+
+console.log(isValid(s));
