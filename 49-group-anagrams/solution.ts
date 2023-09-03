108096843057e51ca9bea78ab0e5516af4c539d7
sjtommylee <sjtommylee@gmail.com>
2023-09-03 15:14:14 -0700

    initial commit

---
+++ 49-group-anagrams/solution.ts
@@ -0,0 +1,10 @@
+let strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+//[["bat"],["nat","tan"],["ate","eat","tea"]]
+
+/** */
+function groupAnagrams(strs: string[]): string[][] {
+  // we can first sort each of the characters
+  // after the sorting, it should look like
+  // ['aet', aet, ant, ...]
+  return [];
+}
