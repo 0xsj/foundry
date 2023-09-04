c7c75cf1d7ef80f5c687d2a7d263ac385b9d6212
sjtommylee <sjtommylee@gmail.com>
2023-09-04 16:55:39 -0700

    python solution

--- 704-binary-search/solution.py
+++ 704-binary-search/solution.py
@@ -4,4 +4,14 @@ class Solution:
     def search(self, nums: List[int], target:int) -> int:
         left = 0
         right = len(nums) - 1
-        
+
+
+        while left <= right:
+            middle = (1 + right) // 2
+            if nums[middle] > target:
+                r = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+        return -1
