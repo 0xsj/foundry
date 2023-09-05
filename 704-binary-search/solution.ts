3d9d188606973efa620b03382eaf5b63c25c2bf1
sjtommylee <sjtommylee@gmail.com>
2023-09-04 17:03:52 -0700

    chore: commit save point

--- 704-binary-search/solution.ts
+++ 704-binary-search/solution.ts
@@ -1,7 +1,21 @@
 const nums = [-1, 0, 3, 5, 9, 12];
 
 function search(nums: number[], target: number): number {
-  for (const num of nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (nums[middle] > target) {
+      right = middle - 1;
+    } else if (nums[middle] < target) {
+      left = middle + 1;
+    } else {
+      return middle;
+    }
   }
+
   return -1;
 }
+
+console.log(search(nums, 9));
