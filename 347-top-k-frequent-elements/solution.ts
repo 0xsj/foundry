c297d5a9e5402f7d89fe0e4557eb4fc4554781a8
sjtommylee <sjtommylee@gmail.com>
2023-09-06 11:37:14 -0700

    chore: commit save point

---
+++ 347-top-k-frequent-elements/solution.ts
@@ -0,0 +1,13 @@
+function topKFrequent(nums: number[], k: number): number[] {
+  const map = new Map();
+
+  for (const num of nums) {
+    const numFrequency = Array(9).fill(0);
+    console.log(numFrequency);
+    //
+  }
+
+  return [];
+}
+
+topKFrequent([], 2);
