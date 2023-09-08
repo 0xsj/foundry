fcfb98b9f30567fc9aed30212be7afbafbcd2d9f
sjtommylee <sjtommylee@gmail.com>
2023-09-08 11:36:06 -0700

    with reduce

--- 1431-greatest-candies/solution.ts
+++ 1431-greatest-candies/solution.ts
@@ -13,6 +13,22 @@ function kidsWithCandies_bruceforce(candies: number[], extraCandies: number) {
   return results;
 }
 
-console.log(kidsWithCandies_bruceforce([2, 3, 5, 1, 3], 3));
-console.log(kidsWithCandies_bruceforce([4, 2, 1, 1, 2], 1));
-console.log(kidsWithCandies_bruceforce([12, 1, 12], 10));
+// console.log(kidsWithCandies_bruceforce([2, 3, 5, 1, 3], 3));
+// console.log(kidsWithCandies_bruceforce([4, 2, 1, 1, 2], 1));
+// console.log(kidsWithCandies_bruceforce([12, 1, 12], 10));
+
+function kidsWithCandies_reduce(candies: number[], extraCandies: number) {
+  const maxValue = Math.max(...candies);
+  const fn = (n: number) => {
+    return n + extraCandies >= maxValue;
+  };
+
+  return candies.reduce<boolean[]>((result, currValue, index) => {
+    result.push(fn(currValue));
+    return result;
+  }, []);
+}
+
+console.log(kidsWithCandies_reduce([2, 3, 5, 1, 3], 3));
+console.log(kidsWithCandies_reduce([4, 2, 1, 1, 2], 1));
+console.log(kidsWithCandies_reduce([12, 1, 12], 10));
