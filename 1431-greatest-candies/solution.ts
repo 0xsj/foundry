75d00a9b97f8ed1c51cf7ed433366dd52ed2cc53
sjtommylee <sjtommylee@gmail.com>
2023-09-08 11:30:16 -0700

    brute force

---
+++ 1431-greatest-candies/solution.ts
@@ -0,0 +1,18 @@
+function kidsWithCandies_bruceforce(candies: number[], extraCandies: number) {
+  const maxValue = Math.max(...candies);
+  const results: boolean[] = [];
+
+  const fn = (n: number) => {
+    return n + extraCandies >= maxValue;
+  };
+
+  for (let i = 0; i < candies.length; i++) {
+    results.push(fn(candies[i]));
+  }
+
+  return results;
+}
+
+console.log(kidsWithCandies_bruceforce([2, 3, 5, 1, 3], 3));
+console.log(kidsWithCandies_bruceforce([4, 2, 1, 1, 2], 1));
+console.log(kidsWithCandies_bruceforce([12, 1, 12], 10));
