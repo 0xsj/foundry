3ae751e9fd9bc9cd5115e03f9a6f0d211be8031e
sjtommylee <sjtommylee@gmail.com>
2023-09-10 13:43:35 -0700

    brute force

--- 238-products-of-array-shelf/solution.ts
+++ 238-products-of-array-shelf/solution.ts
@@ -1,1 +1,18 @@
-function productExceptSelf(nums: number[]): number[] {}
+/**
+ * 1.
+ */
+function productExceptSelf_try1(nums: number[]): number[] {
+  const resultArray = nums.map((_, index) => {
+    return nums.reduce((accumulator, currentValue, currentIndex) => {
+      if (currentIndex === index) {
+        return accumulator;
+      } else {
+        return accumulator * currentValue;
+      }
+    }, 1);
+  });
+
+  return resultArray;
+}
+
+console.log(productExceptSelf_try1([4, 3, 2, 1, 2]));
