95c16a0347528ad9a20ca03663fa2353e5a10eb7
sjtommylee <sjtommylee@gmail.com>
2023-09-10 13:58:27 -0700

    chore: commit save point

--- 238-products-of-array-shelf/solution.ts
+++ 238-products-of-array-shelf/solution.ts
@@ -15,4 +15,22 @@ function productExceptSelf_try1(nums: number[]): number[] {
   return resultArray;
 }
 
+function productExceptSelf_try2(nums: number[]): number[] {
+  const n = nums.length;
+  const result = new Array(n).fill(1);
+
+  let leftProduct = 1;
+  for (let i = 0; i < n; i++) {
+    result[i] *= leftProduct;
+    leftProduct *= nums[i];
+  }
+
+  let rightProduct = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    result[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return result;
+}
 console.log(productExceptSelf_try1([4, 3, 2, 1, 2]));
