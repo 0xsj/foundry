664c957e82449139cb19031bc9a676218aaf5b5c
sjtommylee <sjtommylee@gmail.com>
2023-09-15 21:13:09 -0700

    twosum python bs

--- 1-two-sum/solution.py
+++ 1-two-sum/solution.py
@@ -1,3 +1,18 @@
-# class Solution:
-#     def twoSum():
+from typing import List
+
+nums = [2,7,11,15]
+target = 9
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return [-1, 1]
+
         
+solution = Solution()
+
+print(solution.twoSum(nums, target))
