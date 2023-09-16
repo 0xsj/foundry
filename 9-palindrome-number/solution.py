28d34d0c394df9a7e69d0dd26d7bc290d25587f4
sjtommylee <sjtommylee@gmail.com>
2023-09-15 20:55:25 -0700

    chore: commit save point

--- 9-palindrome-number/solution.py
+++ 9-palindrome-number/solution.py
@@ -1,3 +1,21 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        left = 0
+        right = -1
+
+        # get rid of fail cases negative integers cannot be palindromes
+        if x < 0:
+            return False
+
+        res = str(x).split(" ")[0]
+
+        
+
+        print(res)
+
+
         return False
+    
+solution = Solution()
+
+solution.isPalindrome(121)
