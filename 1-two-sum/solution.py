35fbc4ef1871c88007e1c74912147f224b8457f1
sjtommylee <sjtommylee@gmail.com>
2023-09-15 21:14:12 -0700

    chore: commit save point

--- 1-two-sum/solution.py
+++ 1-two-sum/solution.py
@@ -4,15 +4,22 @@ nums = [2,7,11,15]
 target = 9
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum_try1(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
         for i in range(n):
             for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
         return [-1, 1]
+    
+    def twoSum_try2():
+        return
+    def twoSum_try3():
+        return
+    def twoSum_try4():
+        return
 
         
 solution = Solution()
 
-print(solution.twoSum(nums, target))
+print(solution.twoSum_try1(nums, target))
