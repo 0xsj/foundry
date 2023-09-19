7d50c8d22fb8f8c3a3561cbed8ed03c875ffbafc
sjtommylee <sjtommylee@gmail.com>
2023-09-19 11:44:30 -0700

    9-py solution try 2

--- 9-palindrome-number/solution.py
+++ 9-palindrome-number/solution.py
@@ -6,6 +6,18 @@ class Solution:
         if x_str != x_str[::-1]:
             return False    
         return True
+    def isPalindrome_try2(self, x:int) -> bool:
+        if x < 0:
+            return False
+        original = x
+        reversed = 0
+
+        while (x > 0):
+            lastDigit = x % 10
+            reversed = reversed*10 +  lastDigit
+            x = x // 10
+
+        return original == reversed
     
 solution = Solution()
-print(solution.isPalindrome(121))  # This will print True
+print(solution.isPalindrome_try2(121))  # This will print True
