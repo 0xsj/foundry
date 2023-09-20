c4f9982eec041f872bbddd821990bd08510133fc
sjtommylee <sjtommylee@gmail.com>
2023-09-20 16:02:18 -0700

    1679/try1

--- 1679-max-number-of-k-sum/solution.ts
+++ 1679-max-number-of-k-sum/solution.ts
@@ -1,4 +1,27 @@
 function maxOperations(nums: number[], k: number): number {
+  let count = 0;
+  let left = 0;
+  let right = nums.length - 1;
+
+  // sor the nums
+  const newArray = nums.sort((a, b) => a - b);
+
+  console.log(newArray);
+
+  while (left < right) {
+    const sum = newArray[left] + newArray[right];
+    if (sum === k) {
+      newArray.splice(left, 1);
+      newArray.splice(right - 1, 1);
+      count++;
+      right -= 2;
+    } else if (sum < k) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
   // count variable, to keep in track of the total number of operatios
   // keep a left variable, starting at 0 index
   // keep a right variable or end of the array. nums.length -1
@@ -11,8 +34,13 @@ function maxOperations(nums: number[], k: number): number {
   // repeat
   // if there are no more possible outcomes, meaning none of the remaining numbers add up to k,
   // end and return the count
+  return count;
 }
 
-function maxOperations_try2(nums: number[], k: number): number {}
+maxOperations([1, 4, 3, 2, 5], 6);
 
-function maxOperations_try3(nums: number[], k: number): number {}
+// function maxOperations_try2(nums: number[], k: number): number {
+//   // perhaps its possible to do it with a
+// }
+
+// function maxOperations_try3(nums: number[], k: number): number {}
