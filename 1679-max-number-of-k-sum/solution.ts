7ed65d58ced2703b485cbe67eee03f1bf7b3d19b
sjtommylee <sjtommylee@gmail.com>
2023-09-20 14:58:05 -0700

    initial commit

---
+++ 1679-max-number-of-k-sum/solution.ts
@@ -0,0 +1,8 @@
+function maxOperations(nums: number[], k: number): number {
+  // count variable, to keep in track of the total number of operatios
+  // we are examining the array
+  // we take a look at two "pairs". if nusms[pair1] + nums[pair2] === k,
+  // pop nums[pair1] nums[pair2] out of the array
+  // repeat
+  // if
+}
