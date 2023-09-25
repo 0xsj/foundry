60c4b551bb27c904290149dbfa145d6a8c23977b
sjtommylee <sjtommylee@gmail.com>
2023-09-25 11:56:38 -0700

    with inner join

--- SQL/1757-recyle-low-fat-products/solution.sql
+++ SQL/1757-recyle-low-fat-products/solution.sql
@@ -1,1 +1,7 @@
+-- TRY 1
 SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+
+-- TRY 2
+
+SELECT p.product_id FROM Products as p INNER JOIN Products AS q ON p.product_id = q.product_id AND p.low_fats = 'Y' AND q.recycleable = 'Y'
