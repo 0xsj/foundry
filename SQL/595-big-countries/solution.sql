93924965d819adef89475de81d26ae7016a876cc
sjtommylee <sjtommylee@gmail.com>
2023-09-25 15:04:28 -0700

    initial commit

--- SQL/595-big-countries/solution.sql
+++ SQL/595-big-countries/solution.sql
@@ -7,3 +7,15 @@ FROM World
 WHERE (area >= 3000000 AND population >= 25000000)
 OR (area >= 3000000)
 OR (population >= 25000000);
+
+
+-- TRY 3 UNION
+SELECT name, population, area 
+FROM World 
+WHERE area >= 3000000
+
+UNION
+
+SELECT name, population, area 
+FROM World 
+WHERE population >= 25000000;
