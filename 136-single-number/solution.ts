ca855f49b8dcf13c9c62576108978d845f25efdc
sjtommylee <sjtommylee@gmail.com>
2023-12-02 13:06:49 -0800

    chore: commit save point

--- 136-single-number/solution.ts
+++ 136-single-number/solution.ts
@@ -15,15 +15,48 @@ function singleNumber(nums: number[]): number {
   return 0;
 }
 
+// function singleNumber2(nums: number[]): number {
+//   const seen = new Set<number>();
+//   for (const num of nums) {
+//     if (seen.has(num)) {
+//       seen.delete(num);
+//     } else {
+//       seen.add(num);
+//     }
+//   }
+//   return seen.values().next().value || -1;
+// }
 
 function singleNumber2(nums: number[]): number {
-    const seen = new Set();
-    for(const num of nums) {
-        if
-    }
+  let result = 0;
+
+  for (const num of nums) {
+    result ^= num;
+  }
+
+  return result;
 }
 
-console.log(singleNumber([4, 3, 2, 2, 3]));
-console.log(singleNumber([1, 3, 3]));
-console.log(singleNumber([1]));
-console.log(singleNumber([3, 3, 1]));
+function singleNumber3(nums: number[]): number {
+  const bucket: Record<number, number> = {};
+  nums.forEach((number, index) => {
+    if (bucket[number]) {
+      bucket[number]++;
+    } else {
+      bucket[number] = 1;
+    }
+  });
+
+  for (const key in bucket) {
+    if (bucket[key] == 1) {
+      return parseInt(key);
+    }
+  }
+
+  return 0;
+}
+
+console.log(singleNumber2([4, 3, 2, 2, 3]));
+console.log(singleNumber2([1, 3, 3]));
+console.log(singleNumber2([1]));
+console.log(singleNumber2([3, 3, 1]));
