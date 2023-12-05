364b7b9b53e7415941037fb5a7e286fc6f2d0dd2
sjtommylee <sjtommylee@gmail.com>
2023-12-05 15:13:26 -0800

    chore: commit save point

--- 1920-build-array/solution.ts
+++ 1920-build-array/solution.ts
@@ -1,13 +1,21 @@
+// function buildArray(nums: number[]): number[] {
+//   let ans: number[] = [];
+
+//   for (let i = 0; i < nums.length; i++) {
+//     ans.push(nums[nums[i]]);
+//   }
+
+//   console.log(ans);
+
+//   return ans;
+// }
+
 function buildArray(nums: number[]): number[] {
   let ans: number[] = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    ans.push(nums[nums[i]]);
-  }
-
-  console.log(ans);
-
-  return ans;
+  return nums.map((value) => {
+    nums[value];
+  }) as unknown as number[];
 }
 
 buildArray([0, 2, 1, 5, 3, 4]);
