3b689da3d0b7d5a07c1b53a39b8bcf1ba533ef0a
sjtommylee <sjtommylee@gmail.com>
2023-12-07 14:58:35 -0800

    chore: commit save point

--- 167-twosum-2/solution.ts
+++ 167-twosum-2/solution.ts
@@ -17,4 +17,46 @@ function twoSum(numbers: number[], target: number): number[] {
   return [];
 }
 
-console.log(twoSum(arr, target167));
+// two pointer
+function twoSum2(numbers: number[], target: number): number[] {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    let current = numbers[left] + numbers[right];
+
+    if (current > target) {
+      right -= 1;
+    } else if (current < target) {
+      left += 1;
+    } else {
+      return [left + 1, right + 1];
+    }
+  }
+
+  return [];
+}
+
+// filtered
+function twoSum3(numbers: number[], target: number): number[] {
+  const filtered = numbers.filter((value) => value <= target);
+  let left = 0;
+  let right = filtered.length - 1;
+
+  while (left < right) {
+    let current = filtered[left] + filtered[right];
+    if (current > target) {
+      right -= 1;
+    } else if (current < target) {
+      left += 1;
+    } else {
+      return [left + 1, right + 1];
+    }
+  }
+
+  return [];
+
+  return [];
+}
+
+console.log(twoSum3(arr, target167));
--- 171-excel-spreadsheet/solution.ts
+++ 171-excel-spreadsheet/solution.ts
@@ -5,7 +5,7 @@ function titleToNumber(columnTitle: string): number {
   for (let i = 0; i < columnTitle.length; i++) {
     const value = columnTitle.charCodeAt(i) - "A".charCodeAt(0) + 1;
 
-    output = output + 26 + value;
+    output = output * 26 + value;
   }
 
   //
